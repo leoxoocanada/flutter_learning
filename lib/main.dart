8513f2d6285c0ab63c730e871f3fcb944911c003
Leox <imgafish@gmail.com>
2023-12-24 21:45:59 +0800

    ImageDemo

--- lib/main.dart
+++ lib/main.dart
@@ -40,67 +40,286 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Padding(
+      body: const Padding(
         padding: EdgeInsets.all(20),
-        child: ButtonDemo(),
+        // child: ButtonDemo(),
+        child: ImageDemo(),
       ),
     );
   }
 }
-class ButtonDemo extends StatelessWidget {
+
+class ImageDemo extends StatelessWidget {
+  const ImageDemo({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Column(children: [
-          ElevatedButton(
-            child: Text('点赞'),
-            onPressed: () {
-              print('xxxxx');
-            }
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          // Image(
+          //   image: AssetImage('images/logo.png'),
+          //   width: 200,
+          // ),
+          // Image.asset('images/logo.png', width: 200,),
+          // Image(
+          //   image: NetworkImage('https://www.baidu.com/img/flexible/logo/pc/result.png'),
+          //   width: 200,
+          // ),
+          // Image.network('https://www.baidu.com/img/flexible/logo/pc/result.png', width: 200,),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 200,
+          //     height: 100,
+          //     fit: BoxFit.contain
+          //   ),
+          // ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 200,
+          //     height: 100,
+          //     fit: BoxFit.cover
+          //   ),
+          // ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 200,
+          //     height: 100,
+          //     fit: BoxFit.fill
+          //   ),
+          // ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 200,
+          //     height: 100,
+          //     fit: BoxFit.fitHeight
+          //   ),
+          // ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 200,
+          //     height: 100,
+          //     fit: BoxFit.fitWidth
+          //   ),
+          // ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 200,
+          //     height: 100,
+          //     fit: BoxFit.none
+          //   ),
+          // ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 200,
+          //     height: 100,
+          //     fit: BoxFit.scaleDown
+          //   ),
+          // ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 200,
+          //     height: 100,
+          //     color: Colors.blue,
+          //     colorBlendMode: BlendMode.difference,
+          //     fit: BoxFit.scaleDown
+          //   ),
+          // ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 200,
+          //     height: 100,
+          //     color: Colors.blue,
+          //     colorBlendMode: BlendMode.color,
+          //     fit: BoxFit.scaleDown
+          //   ),
+          // ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 300,
+          //     height: 300,
+          //     fit: BoxFit.scaleDown,
+          //     repeat: ImageRepeat.noRepeat,
+          //   ),
+          // ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 300,
+          //     height: 300,
+          //     fit: BoxFit.scaleDown,
+          //     repeat: ImageRepeat.repeat,
+          //   ),
+          // ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 300,
+          //     height: 300,
+          //     fit: BoxFit.scaleDown,
+          //     repeat: ImageRepeat.repeatX,
+          //   ),
+          // ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     border: Border.all(color: Colors.red, width: 10),
+          //   ),
+          //   child: Image(
+          //     image: AssetImage('images/logo.png'),
+          //     width: 300,
+          //     height: 300,
+          //     fit: BoxFit.scaleDown,
+          //     repeat: ImageRepeat.repeatY,
+          //   ),
+          // ),
+          // Text(
+          //   '\uE03e', 
+          //   style: TextStyle(
+          //     fontFamily: 'MaterialIcons',
+          //     fontSize: 200,
+          //     color: Colors.green,
+          //   ),
+          // ),
+          // Icon(
+          //   Icons.accessible, 
+          //   size: 200, 
+          //   color: Colors.green,
+          // ),
+          // Text(
+          //   '前端大神勇者无敌', 
+          //   style: TextStyle(
+          //     fontFamily: 'DFDK',
+          //     fontSize: 80,
+          //   )
+          // ),
+          Text(
+            '\ue6a8', 
+            style: TextStyle(
+              fontFamily: 'image_haven_font',
+              fontSize: 80,
+              color: Colors.red,
+            )
           ),
-          TextButton(
-            child: Text('点赞'),
-            onPressed: () {
-              print('xxxxx');
-            }
+          Text(
+            '\ue66c', 
+            style: TextStyle(
+              fontFamily: 'image_haven_font',
+              fontSize: 80,
+              color: Colors.red,
+            )
           ),
-          OutlinedButton(
-            child: Text('点赞'),
-            onPressed: () {
-              print('xxxxx');
-            }
-          ),
-          IconButton(
-            icon: Icon(Icons.thumb_up),
-            onPressed: () {
-              print('xxxxx');
-            }
-          ),
-          ElevatedButton.icon(
-            icon: Icon(Icons.thumb_up),
-            label: Text('点赞'),
-            onPressed: () {
-              print('xxxxx');
-            }
-          ),
-          TextButton.icon(
-            icon: Icon(Icons.thumb_up),
-            label: Text('点赞'),
-            onPressed: () {
-              print('xxxxx');
-            }
-          ),
-          OutlinedButton.icon(
-            icon: Icon(Icons.thumb_up),
-            label: Text('点赞'),
-            onPressed: () {
-              print('xxxxx');
-            }
-          ),
-        ],)
-      );
+          Icon(MyIcon.lu, size: 80, color: Colors.red,),
+        ],
+      )
+    );
   }
 }
+
+class MyIcon {
+  static const IconData lu = IconData(0xe66c, fontFamily: 'image_haven_font', matchTextDirection: true);
+}
+
+// class ButtonDemo extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         child: Column(children: [
+//           ElevatedButton(
+//             child: Text('点赞'),
+//             onPressed: () {
+//               print('xxxxx');
+//             }
+//           ),
+//           TextButton(
+//             child: Text('点赞'),
+//             onPressed: () {
+//               print('xxxxx');
+//             }
+//           ),
+//           OutlinedButton(
+//             child: Text('点赞'),
+//             onPressed: () {
+//               print('xxxxx');
+//             }
+//           ),
+//           IconButton(
+//             icon: Icon(Icons.thumb_up),
+//             onPressed: () {
+//               print('xxxxx');
+//             }
+//           ),
+//           ElevatedButton.icon(
+//             icon: Icon(Icons.thumb_up),
+//             label: Text('点赞'),
+//             onPressed: () {
+//               print('xxxxx');
+//             }
+//           ),
+//           TextButton.icon(
+//             icon: Icon(Icons.thumb_up),
+//             label: Text('点赞'),
+//             onPressed: () {
+//               print('xxxxx');
+//             }
+//           ),
+//           OutlinedButton.icon(
+//             icon: Icon(Icons.thumb_up),
+//             label: Text('点赞'),
+//             onPressed: () {
+//               print('xxxxx');
+//             }
+//           ),
+//         ],)
+//       );
+//   }
+// }
+
 // class _MyHomePageState extends State<MyHomePage> {
 //   int _counter = 0;
 
